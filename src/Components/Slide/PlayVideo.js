8261280687b09b19c7dc6986f29363d1c74a5c5e
sathiswar <slsjkr@gmail.com>
2024-10-26 15:13:10 +0530

    mmm

--- src/Components/Slide/PlayVideo.js
+++ src/Components/Slide/PlayVideo.js
@@ -1,11 +1,19 @@
 import { useLocation } from "react-router-dom";
-
+import './PlayVideo.css'
 const PlayVideo = () => {
   const location = useLocation();
-  const { videoUrl, showData } = location.state || {}; // Extract videoUrl and showData for the clicked item
+  const { videoUrl, showData } = location.state || {}; 
+
+  const stripAndTruncateSummary = (summary) => {
+    const cleanSummary = summary.replace(/<\/?[^>]+(>|$)/g, '');  
+    return cleanSummary 
+  };
+
 
   return (
-    <div>
+    <>
+    <div className="playvideo-section">
+      <div className="play-video">
       {videoUrl ? (
         <iframe
           width="74%"
@@ -20,18 +28,37 @@ const PlayVideo = () => {
       ) : (
         <p>No video available</p>
       )}
+      </div>
 
       {showData ? (
         <>
-        <div className="" style={{ marginLeft: "22px", display:'flex' }}>
-          <h2>{showData.show.name}</h2> 
-          <p>{showData.show.language}</p>
-          </div>  
-        </>
+        <div style={{ marginLeft: "22px", display: 'flex', alignItems: 'center', gap: '30px' }}>
+          <h2>{showData.show.name} </h2>
+          <p>|</p>
+          <p style={{ fontSize: '1.2rem'  }}>{showData.show.language}  </p>
+          <p>|</p>
+          <p style={{ fontSize: '1.2rem' }}>{showData.show.webChannel.name}  </p>
+        </div>
+
+ 
+        <hr style={{ fontSize: '1.2rem' , width:"73%",position: "absolute",marginLeft:"28px", marginTop:"3px",color:"#333335"}}/>
+
+        <div style={{ marginLeft: "22px", display: 'flex', flexDirection:'column',  gap: '10px' }}>
+        <p style={{ fontSize: '1.2rem' }}>Summary : </p>
+        <p style={{ fontSize: '1.2rem' , width:"70%"}}>{stripAndTruncateSummary(showData.show.summary)}
+        <p style={{ fontSize: '1.2rem'  }}>Language : {showData.show.language}</p>
+        <p style={{ fontSize: '1.2rem'  }}>Type : {showData.show.type}</p>
+        <p style={{ fontSize: '1.2rem'  }}>Genre : {showData.show.genres}</p>
+        <p style={{ fontSize: '1.2rem' }}>Premiered: {showData.show.premiered}</p>
+        <p style={{ fontSize: '1.2rem' }}>Ended: {showData.show.ended}</p>
+        </p>
+      </div>
+      </>
       ) : (
         <p>No show data available</p>
       )}
     </div>
+    </>
   );
 };
 
